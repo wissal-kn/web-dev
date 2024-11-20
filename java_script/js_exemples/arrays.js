f24db6efc0e2f229ce89b99b76d462a06f7377ef
wissal <wissalerahmenekanouni@gmail.com>
2024-11-20 10:23:36 +0100

    fek

---
+++ java_script/js_exemples/arrays.js
@@ -0,0 +1,65 @@
+
+//-------------arrays in js exemples---------------------//
+// - Declare an empty array.
+const emptyArray=[];
+// - Declare an array with more than 5 number of elements.
+const arr=[0,1,2,3,4];
+
+// - Find the length of your array use arr.length
+const arrayLength=arr.length;
+// - Get the first item, the middle item and the last item of the array.
+const middArray=arrayLength/2;
+const fistItem=arr.findIndex(0);
+const middItem=arr.findIndex(middArray);
+const lastArray=arrayLenghth-1;
+const lastItem=arr.findIndex(lastArray);
+
+// - Declare an array called mixedDataTypes, put different data types in the array and find the length of the array the array size should be greater than 5.
+
+const mixedDataTypes=[apple,23,True,false,apple];
+console.log(mixedDataTypes);  
+
+// - Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon.
+const itCompanies=[Facebook,Google,Microsoft,Apple,IBM,Oracle,Amazon]
+  
+// - Print the array using console.log().
+console.log(itCompanies);
+// - Print the number of companies in the array.
+console.log(itCompanies.length);
+// - Print the first company, middle and last company.
+const fistCompany=arr.findIndex(0);
+console.log(firstCompany);
+
+
+//---------------objects in js exemple-----------------------//
+// create an empty object named dog
+// Print the dog object on the console
+// Add name, NumberOfLegs, color and age properties for the dog object.
+// console.log() name, legs, color and age value from the dog object.
+// Set new properties the dog object: breed.
+
+
+
+- Iterate 0 to 10 using for loop, do the same using while and do while loop.
+- Use loop to print the following pattern:
+  0 x 0 = 0
+  1 x 1 = 1
+  2 x 2 = 4
+  3 x 3 = 9
+  4 x 4 = 16
+  5 x 5 = 25
+  6 x 6 = 36
+  7 x 7 = 49
+  8 x 8 = 64
+  9 x 9 = 81
+  10 x 10 = 100
+
+
+Use for loop to iterate from 0 to 100 and print only even numbers.
+Hamza CHEBBAH
+- Use for loop to iterate from 0 to 100 and print only odd numbers.
+Hamza CHEBBAH
+- Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+
+
+
